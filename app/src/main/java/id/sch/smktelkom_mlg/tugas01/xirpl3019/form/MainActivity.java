805ef2b60c95b8871b256090b0a3f497f25b8a54
Mufidatun Nabilah <mufida.nabilah3@gmail.com>
2016-10-05 05:37:39 +0700

    Final commit

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3019/form/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3019/form/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
-
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.EditText;
